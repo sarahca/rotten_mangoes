251eefaa8cac3844c8d707acce935d564d5d16a1
sarahca <s.canadabc@gmail.com>
2014-11-25 14:48:57 -0800

    first commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,44 @@
+class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+    if @movie.update_attributes(movie_params)
+      redirect_to movies_patth
+    else
+      render: edit
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  protected
+  def movie_params
+    params.require(:movie).permit(
+      :title, :director, :runtime_in_minutes, :description, :poster_image_url, :release_date
+    )
+  end
+end
+
